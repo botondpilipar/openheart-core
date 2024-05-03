28cec4ef23909a6b05249b4a94fd9f10bfeb122a
botondpilipar <pilipar.botond@gmail.com>
2024-05-03 22:03:44 +0200

    Introduce Meson as main build tool
    Rebuild project structure and dependency
    management purely using meson.
    
    Include log.c as a subproject, as it does not have
    its primary build system shipped with the minimal
    library

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
 #include <curl/curl.h>
-#include <log.h>
 #include <kernel_attributes.h>
 
 int main(int argc, char** argv)
 {
     KernelAttr user_kernel_attributes;
     printf("OpenHeart Core\n");
+    return 0;
 }
---
+++ pch/core_pch.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <glib.h>
+#include <curl/curl.h>
